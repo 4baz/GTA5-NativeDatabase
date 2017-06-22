3116f7be4bc3675c5fba1877a08f10711f63c6c8
Gelox_ <macoonier@gmail.com>
2017-06-22 18:07:55 +0200

    Added copy name/hash to native info box Added icon Disabled ability to select excluding native list section

--- storage/scripts/ndb.js
+++ storage/scripts/ndb.js
@@ -102,11 +102,11 @@ function closeNamespaceTab(namespace) {
     })
 }
 
-function openNamespaceTab(namespace) {
+async function openNamespaceTab(namespace) {
     const ele = document.getElementById("ns-" + namespace);
     const natives = getNativeObjects(namespace);
 
-    let htmlCode = ele.parentElement.innerHTML + "<ul id='na-" + namespace + "'>";
+    let htmlCode = ele.parentElement.innerHTML + "<ul id='na-" + namespace + "' class='natives'>";
 
     let i = 0;
 
@@ -161,22 +161,31 @@ function openFunctionInformation(namespace, functionHash, functionDeclHTML) {
     const nativeObj = jsonData[namespace][functionHash];
 
     let newHTML = "<div style='padding-left: 1%;'><div class='funcbox'>" +
-        "<h2>" + namespace + "::" + name + "</h2><hr>" +
+        "<p style='font-weight: bold; font-size: 20px;'>" + namespace + "::" + name + "</p><hr>" +
         functionDeclHTML + "<hr>";
 
-    newHTML += "<p style='white-space: pre-wrap;'>";
+    newHTML += "<p style='white-space: pre-wrap; display: inline;'><br>";
 
     if (hasComment(nativeObj)) {
         newHTML += nativeObj.comment;
     } else newHTML += "No comment available";
 
-    newHTML += "</p></div></div>";
+    newHTML += "<br><br></p><div id='cpn-" + name + "' class='buttonbox' style='margin-right: 9%;'>Copy Name</div><div id='cph-" + name + "' class='buttonbox'>Copy Hash</div></div></div>";
 
     ele.innerHTML += newHTML;
 
     document.getElementById("func-" + functionHash).addEventListener("click", function() {
         closeFunctionInformation(ele, namespace, functionHash, functionDeclHTML);
     });
+
+    document.getElementById("cpn-" + name).addEventListener("click", function () {
+        copyTextToClipboard(name);
+    });
+
+    document.getElementById("cph-" + name).addEventListener("click", function () {
+        copyTextToClipboard(functionHash);
+    });
+
 }
 
 function closeFunctionInformation(funcElement, namespace, funcHash, funcDeclHTML) {
@@ -187,18 +196,56 @@ function closeFunctionInformation(funcElement, namespace, funcHash, funcDeclHTML
     })
 }
 
-function joaat(key) {
-    let hash = 0, i = 0;
+function copyTextToClipboard(text) {
+    const textArea = document.createElement("textarea");
 
-    for (i = 0; i < key.length; i++) {
-        hash += key.charCodeAt(i);
-        hash += (hash << 10);
-        hash ^= (hash >> 6);
-    }
-    hash += (hash << 3);
-    hash ^= (hash >> 11);
-    hash += (hash << 15);
-    return hash >>> 0;
+    //
+    // *** This styling is an extra step which is likely not required. ***
+    //
+    // Why is it here? To ensure:
+    // 1. the element is able to have focus and selection.
+    // 2. if element was to flash render it has minimal visual impact.
+    // 3. less flakyness with selection and copying which **might** occur if
+    //    the textarea element is not visible.
+    //
+    // The likelihood is the element won't even render, not even a flash,
+    // so some of these are just precautions. However in IE the element
+    // is visible whilst the popup box asking the user for permission for
+    // the web page to copy to the clipboard.
+    //
+
+    // Place in top-left corner of screen regardless of scroll position.
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+
+    // Ensure it has a small width and height. Setting to 1px / 1em
+    // doesn't work as this gives a negative w/h on some browsers.
+    textArea.style.width = '2em';
+    textArea.style.height = '2em';
+
+    // We don't need padding, reducing the size if it does flash render.
+    textArea.style.padding = 0;
+
+    // Clean up any borders.
+    textArea.style.border = 'none';
+    textArea.style.outline = 'none';
+    textArea.style.boxShadow = 'none';
+
+    // Avoid flash of white box if rendered for any reason.
+    textArea.style.background = 'transparent';
+
+
+    textArea.value = text;
+
+    document.body.appendChild(textArea);
+
+    textArea.select();
+
+
+    document.execCommand('copy');
+
+    document.body.removeChild(textArea);
 }
 
 async function init() {
@@ -208,7 +255,6 @@ async function init() {
         await sleep(1);
     }
 
-    console.log(joaat("get_player_ped"))
     let namespaces = "";
     let nsCount = 0, nCount = 0, cCount = 0, kCount = 0;
     const v = getNamespaces();
